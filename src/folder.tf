e7422905163f24e124eabbc17e6b6f517b83d07f
Harmelodic <matt@harmelodic.com>
2022-04-18 11:03:12 +0200

    Setup workload identity and create a folder

--- src/folder.tf
+++ src/folder.tf
@@ -1,10 +1,4 @@
-#resource "google_folder" "personal" {
-#  display_name = "Personal - ${title(terraform.workspace)}"
-#  parent       = data.google_organization.harmelodic_com.id
-#}
-#
-#resource "google_folder_iam_member" "operations-automation" {
-#  folder  = google_folder.personal.id
-#  member  = "serviceAccount:${data.google_service_account.automation.email}"
-#  role    = "roles/owner"
-#}
+resource "google_folder" "personal" {
+  display_name = "Personal - ${title(terraform.workspace)}"
+  parent       = data.google_organization.harmelodic_com.id
+}
