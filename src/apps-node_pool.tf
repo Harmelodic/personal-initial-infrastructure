9eecf1c40db032862c4946b9d6092914e506475e
Harmelodic <matt@harmelodic.com>
2022-05-28 17:32:52 +0200

    Create gke and node pool without explicit service account

--- src/apps-node_pool.tf
+++ src/apps-node_pool.tf
@@ -16,47 +16,47 @@ variable "apps_gke_node_pool_machine_type" {
   type        = string
 }
 
-#resource "google_container_node_pool" "apps" {
-#  cluster        = google_container_cluster.apps.name
-#  location       = var.apps_gke_location
-#  name           = "apps"
-#  node_count     = var.apps_gke_node_count
-#  node_locations = var.apps_gke_node_locations
-#  project        = google_project.apps.project_id
-#
-#  management {
-#    auto_repair  = true
-#    auto_upgrade = true
-#  }
-#
-#  node_config {
-#    disk_size_gb    = 100
-#    disk_type       = "pd-standard"
-#    image_type      = "cos_containerd"
-#    local_ssd_count = 0
-#    machine_type    = var.apps_gke_node_pool_machine_type
-#    preemptible     = false
-#
-#    labels = {
-#      environment = terraform.workspace
-#    }
-#
-#    metadata = {
-#      disable-legacy-endpoints = true
-#    }
-#
-#    tags = [
-#      terraform.workspace
-#    ]
-#
-#    shielded_instance_config {
-#      enable_integrity_monitoring = true
-#      enable_secure_boot          = true
-#    }
-#  }
-#
-#  upgrade_settings {
-#    max_surge       = 2
-#    max_unavailable = 1
-#  }
-#}
+resource "google_container_node_pool" "apps" {
+  cluster        = google_container_cluster.apps.name
+  location       = var.apps_gke_location
+  name           = "apps"
+  node_count     = var.apps_gke_node_count
+  node_locations = var.apps_gke_node_locations
+  project        = google_project.apps.project_id
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  node_config {
+    disk_size_gb    = 100
+    disk_type       = "pd-standard"
+    image_type      = "cos_containerd"
+    local_ssd_count = 0
+    machine_type    = var.apps_gke_node_pool_machine_type
+    preemptible     = false
+
+    labels = {
+      environment = terraform.workspace
+    }
+
+    metadata = {
+      disable-legacy-endpoints = true
+    }
+
+    tags = [
+      terraform.workspace
+    ]
+
+    shielded_instance_config {
+      enable_integrity_monitoring = true
+      enable_secure_boot          = true
+    }
+  }
+
+  upgrade_settings {
+    max_surge       = 2
+    max_unavailable = 1
+  }
+}
