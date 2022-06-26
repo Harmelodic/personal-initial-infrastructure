4468839bfc4010a19bca8a6feb73ed8145867878
Harmelodic <matt@harmelodic.com>
2022-06-26 21:15:04 +0200

    Adding an extra value

--- src/apps-harmelodic-dns.tf
+++ src/apps-harmelodic-dns.tf
@@ -74,6 +74,7 @@ resource "google_dns_record_set" "harmelodic_com_txt" {
 
   rrdatas = [
     "google-site-verification=jXVba9WLVzprbkW4EpS3vtWL5-2YH03AwSc8sprMfSU",
-    "keybase-site-verification=sDm605nNkmQuRsuciUxr9KmDkMgBVUKD5Ea38C_8L4w"
+    "keybase-site-verification=sDm605nNkmQuRsuciUxr9KmDkMgBVUKD5Ea38C_8L4w",
+    "extra-custom-value=hello-matt"
   ]
 }
