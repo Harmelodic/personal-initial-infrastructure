892346623d1e35e28c164f891a73872167baafe5
Harmelodic <matt@harmelodic.com>
2022-07-02 21:32:34 +0200

    regranting artifacts permissions to both

--- src/artifacts-harmelodic.tf
+++ src/artifacts-harmelodic.tf
@@ -9,10 +9,15 @@ resource "google_artifact_registry_repository" "harmelodic_docker" {
 }
 
 resource "google_artifact_registry_repository_iam_member" "gke_service_account" {
+  for_each = toset([
+    "serviceAccount:${google_project.apps.number}@cloudservices.gserviceaccount.com",
+    "serviceAccount:${module.harmelodic_website_workload.google_service_account.email}"
+  ])
+
   provider   = google-beta
   project    = google_artifact_registry_repository.harmelodic_docker.project
   location   = google_artifact_registry_repository.harmelodic_docker.location
   repository = google_artifact_registry_repository.harmelodic_docker.name
   role       = "roles/artifactregistry.reader"
-  member     = "serviceAccount:${google_project.apps.number}@cloudservices.gserviceaccount.com"
+  member     = each.value
 }
