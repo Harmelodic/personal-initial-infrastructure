daa70879529b76b4cb74ef547e15e6ad1217fb0b
Harmelodic <matt@harmelodic.com>
2022-07-02 18:26:24 +0200

    harmelodic

--- src/artifacts-harmelodic.tf
+++ src/artifacts-harmelodic.tf
@@ -1,7 +1,7 @@
 resource "google_artifact_registry_repository" "harmelodic_docker" {
   provider = google-beta
 
-  description   = "Docker repository for Personal images"
+  description   = "Docker repository for Harmelodic images"
   format        = "DOCKER"
   project       = google_project.artifacts.project_id
   repository_id = "harmelodic"
