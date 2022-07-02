0e2524073e9b4c8fdc128cbee861015a9dc83384
Harmelodic <matt@harmelodic.com>
2022-07-02 23:00:29 +0200

    gke node service agent to be reader

--- src/apps-harmelodic-workloads.tf
+++ src/apps-harmelodic-workloads.tf
@@ -10,12 +10,3 @@ module "harmelodic_website_workload" {
   namespace  = kubernetes_namespace.harmelodic.metadata.0.name
   project_id = google_project.apps.project_id
 }
-
-resource "google_artifact_registry_repository_iam_member" "harmelodic_website" {
-  provider   = google-beta
-  project    = google_artifact_registry_repository.harmelodic_docker.project
-  location   = google_artifact_registry_repository.harmelodic_docker.location
-  repository = google_artifact_registry_repository.harmelodic_docker.name
-  role       = "roles/artifactregistry.reader"
-  member     = "serviceAccount:${module.harmelodic_website_workload.google_service_account.email}"
-}
--- src/artifacts-harmelodic.tf
+++ src/artifacts-harmelodic.tf
@@ -7,3 +7,12 @@ resource "google_artifact_registry_repository" "harmelodic_docker" {
   project       = google_project.artifacts.project_id
   repository_id = "harmelodic"
 }
+
+resource "google_artifact_registry_repository_iam_member" "gke_node_service_agent" {
+  provider   = google-beta
+  project    = google_artifact_registry_repository.harmelodic_docker.project
+  location   = google_artifact_registry_repository.harmelodic_docker.location
+  repository = google_artifact_registry_repository.harmelodic_docker.name
+  role       = "roles/artifactregistry.reader"
+  member     = "serviceAccount:service-${google_project.apps.number}@gcp-sa-gkenode.iam.gserviceaccount.com"
+}
