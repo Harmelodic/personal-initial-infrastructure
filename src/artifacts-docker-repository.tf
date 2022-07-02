cbf13039d63e721c8df8da89093bbff4e0e4f4d1
Harmelodic <matt@harmelodic.com>
2022-07-02 17:54:54 +0200

    moving provider to the top

--- src/artifacts-docker-repository.tf
+++ src/artifacts-docker-repository.tf
@@ -1,7 +1,8 @@
 resource "google_artifact_registry_repository" "docker" {
+  provider = google-beta
+
   description   = "Docker repository for Personal images"
   format        = "DOCKER"
   project       = google_project.artifacts.project_id
-  provider      = google-beta
   repository_id = "personal"
 }
