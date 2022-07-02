be796620eaa0c8ce77a8ae7ab243bd1432a45caf
Harmelodic <matt@harmelodic.com>
2022-07-02 16:15:22 +0200

    rename to just cert-manager

--- src/apps-cert-manager-service-account.tf
+++ src/apps-cert-manager-service-account.tf
@@ -1,14 +1,14 @@
-resource "google_service_account" "cert_manager_dns01_solver" {
+resource "google_service_account" "cert_manager" {
   project    = google_project.apps.project_id
-  account_id = "dns01-solver"
+  account_id = "cert-manager"
 }
 
-resource "google_project_iam_member" "cert_manager_dns01_solver" {
+resource "google_project_iam_member" "cert_manager" {
   for_each = toset([
     "roles/dns.admin"
   ])
 
-  member  = "serviceAccount:${google_service_account.cert_manager_dns01_solver.email}"
+  member  = "serviceAccount:${google_service_account.cert_manager.email}"
   project = google_project.host.project_id
   role    = each.key
 }
@@ -17,5 +17,5 @@ resource "google_service_account_iam_member" "cert_manager_workload_identity" {
   member             = "serviceAccount:${google_project.apps.project_id}.svc.id.goog[cert-manager/cert-manager]"
   project            = google_project.apps.project_id
   role               = "roles/iam.workloadIdentityUser"
-  service_account_id = google_service_account.cert_manager_dns01_solver.name
+  service_account_id = google_service_account.cert_manager.name
 }
