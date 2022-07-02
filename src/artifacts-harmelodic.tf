86cce0bc32ceef6ce0f3a5646421601ca0f534fa
Harmelodic <matt@harmelodic.com>
2022-07-02 19:14:27 +0200

    Removing broken repository

--- src/artifacts-harmelodic.tf
+++ src/artifacts-harmelodic.tf
@@ -1,8 +1,8 @@
-resource "google_artifact_registry_repository" "harmelodic_docker" {
-  provider = google-beta
-
-  description   = "Docker repository for Harmelodic images"
-  format        = "DOCKER"
-  project       = google_project.artifacts.project_id
-  repository_id = "harmelodic"
-}
+#resource "google_artifact_registry_repository" "harmelodic_docker" {
+#  provider = google-beta
+#
+#  description   = "Docker repository for Harmelodic images"
+#  format        = "DOCKER"
+#  project       = google_project.artifacts.project_id
+#  repository_id = "harmelodic"
+#}
