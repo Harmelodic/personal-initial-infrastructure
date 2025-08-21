493fd05b219bb256d9d139676ac957b34db6071d
Harmelodic <matt@harmelodic.com>
2025-08-22 01:07:04 +0200

    Skip final two rules

--- src/artifacts-harmelodic.tf
+++ src/artifacts-harmelodic.tf
@@ -1,4 +1,5 @@
 resource "google_artifact_registry_repository" "harmelodic_docker" {
+  # checkov:skip=CKV_GCP_84: TODO: Review need for CSEK / CMEK
   description   = "Docker repository for Harmelodic images"
   format        = "DOCKER"
   location      = var.region
--- src/artifacts-mary-website.tf
+++ src/artifacts-mary-website.tf
@@ -1,4 +1,5 @@
 resource "google_artifact_registry_repository" "mary_website_docker" {
+  # checkov:skip=CKV_GCP_84: TODO: Review need for CSEK / CMEK
   description   = "Docker repository for Mary Website images"
   format        = "DOCKER"
   location      = "asia-southeast1"
